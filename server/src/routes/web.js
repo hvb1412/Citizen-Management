038f2e53794b7b64fea5bf51d137ca47e904a705
Hoang Van Binh <hoangvanbinh141205@gmail.com>
2025-12-22 13:52:19 +0700

    feat done BE-18

--- server/src/routes/web.js
+++ server/src/routes/web.js
@@ -19,6 +19,8 @@ import {
 } from "../controllers/personController.js";
 // --- AUTH CONTROLLER --- 
 import authController from "../controllers/authController.js";
+// --- USER CONTROLLER ---
+import userController from "../controllers/userController.js";
 // --- CHECK TOKEN ---
 import verifyToken from "../middleware/authMiddleware.js";
 // --- CHECK ROLE ---
@@ -29,12 +31,28 @@ import pool from "../config/db.js";
 const router = express.Router();
 let initWebRoutes = (app) => {
 
-    // ---- AUTH ROUTE ----
-    router.post("/api/v1/auth/register", verifyToken, checkRole(['admin']), authController.handleRegister);
+    // ---- AUTH ROUTE ---- (Admin)
+    // POST: Tạo 1 tài khoản Admin mới (Cái này cực kỳ nguy hiểm => Tạo ra để Group test => Cần xóa đi khi hoàn thiện project)
+    router.post("/api/v1/auth/register", authController.handleRegister);
+    // POST: Login cho tất cả các User
     router.post("/api/v1/auth/login", authController.handleLogin);
+    // GET: Lấy thông tin của cá nhân User đã đăng nhập
     router.get("/api/v1/auth/me", verifyToken, authController.getMe);
+    
+    // ---- USER ROUTE ---- (Admin)
+    // GET: Admin lấy danh sách tất cả các Users
+    router.get("/api/v1/get-all-users", verifyToken, checkRole(['admin']), userController.handleGetAllUsers);
+    // POST: Admin tạo User mới cho cấp dưới
+    router.post("/api/v1/create-user", verifyToken, checkRole(['admin']), userController.handleCreateUser);
+    // PUT: Admin sửa User (Reset pass, đổi quyền...)
+    router.put("/api/v1/update-user/:id", verifyToken, checkRole(['admin']), userController.handleUpdateUser);
+    // DELETE: Admin xóa user
+    router.delete("/api/v1/delete-user/:id", verifyToken, checkRole(['admin']), userController.handleDeleteUser);
+    
     // ---- Update & Logout ---
+    // PUT: Cá nhân User update (full_name || password) => Không khuyến khích, nên để Admin quản lý tất cả
     router.put("/api/v1/auth/updateProfile", verifyToken, authController.handleUpdateProfile);
+    // POST: Logout cho tất cả Users
     router.post("/api/v1/auth/logout", verifyToken, authController.handleLogout);
 
     router.post("/api/v1/ho-khau", createHousehold);
